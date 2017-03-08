dad5079fdf8960316668fdce1264e12b5c71a12f
hicrystal <295247343@qq.com>
2017-03-08 22:10:45 +0800

    端口http 连接前后端数据

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var koa =require('koa');
 var controller=require('koa-route');
 var app=koa();
+var service= require('./service/webAppService.js');
 //渲染
 var views=require('co-views');
 var render= views('./view',{
@@ -16,5 +17,22 @@ app.use(controller.get('/ejs_test',function*(){
 	this.set('Cache-Control','no-cache');
 	this.body = yield render('test',{test:'biubiu'});
 }));
+
+
+app.use(controller.get('/api_test',function*(){
+	this.set('Cache-Control','no-cache');
+	this.body = service.get_test_data();
+}));
+
+app.use(controller.get('/ajax/search',function*(){
+	this.set('Cache-Control','no-cache');
+	var queryString=require('queryString');
+	var params= queryString.parse(this.req._parsedUrl.query);
+	var start=params.start;
+	var end=params.end;
+	var keyword=params.keyword;
+	this.body = yield service.get_search_data(start,end,keyword);
+}));
+
 app.listen(3001);
 console.log('mb');
